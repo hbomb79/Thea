1885a2ce148d4a6a619e79bb150ef1325f2ad310
Harry Felton <harryfelton12@gmail.com>
2021-07-22 18:43:31 +1200

    chore: added comments for Queue components

--- site/src/components/QueueItem.svelte
+++ site/src/components/QueueItem.svelte
@@ -1,4 +1,12 @@
 <script context="module" lang="ts">
+// This module just exports some interfaces that we want
+// to be able to use in other components.
+
+// QueueTroubleInfo represents the data we receive
+// from the Go server regarding the Title info for
+// our queue item. This information will be
+// unavailable until the title formatting
+// has been completed.
 export interface QueueTitleInfo {
     Title: string
     Episodic: boolean
@@ -8,6 +16,9 @@ export interface QueueTitleInfo {
     Resolution: string
 }
 
+// QueueOmdbInfo represents the information about a queue item
+// from the go server - this data will be unavailable until
+// a worker on the go server has queried OMDB for this information
 export interface QueueOmdbInfo {
     Title: string
     plot: string
@@ -21,10 +32,14 @@ export interface QueueOmdbInfo {
     Genre: string[]
 }
 
+// QueueTroubleInfo is the information regarding a trouble
+// state from the Go server
 export interface QueueTroubleInfo {
     message: string
 }
 
+// QueueDetails is a single interface that extends the definition
+// given by QueueItem, by appending the three above interfaces to it.
 export interface QueueDetails extends QueueItem {
     title_info: QueueTitleInfo
     omdb_info:  QueueOmdbInfo
@@ -45,12 +60,24 @@ import OmdbPanel from "./panels/OmdbPanel.svelte";
 import FfmpegPanel from "./panels/FfmpegPanel.svelte";
 import DatabasePanel from "./panels/DatabasePanel.svelte";
 
+// The queueInfo we're wanting to display from the parent component.
+export let queueInfo:QueueItem
+
+// The enhanced version of the above queueInfo, populated after the component
+// has been mounted by commander (QUEUE_DETAILS websocket command)
+let details:QueueDetails = null
+
+// The state of this component, affected by the websocket
+// packets that we're receiving from commander.
+// This enum is used to control what HTML content we're displaying
 enum ComponentState {
     LOADING,
     ERR,
     COMPLETE
 }
 
+// The page/panel we're wanting to view inside this component,
+// affected by user interaction (e.g. on:click events)
 enum ComponentPage {
     OVERVIEW,
     TITLE,
@@ -60,14 +87,13 @@ enum ComponentPage {
     TROUBLE
 }
 
-
-export let queueInfo:QueueItem
-
+// The initial state/page of the component
 let state = ComponentState.LOADING
 let page = ComponentPage.OVERVIEW
-let details:QueueDetails = null
-const getDetails = function() {
-    // Get enhanced details of the queue item
+
+
+// Get enhanced details of the queue item
+onMount(() => {
     commander.sendMessage({
         type: SocketMessageType.COMMAND,
         title: "QUEUE_DETAILS",
@@ -86,9 +112,19 @@ const getDetails = function() {
 
         return false
     })
-}
 
-const getStageStr = function(stage:number): string {
+    // TODO REMOVE THIS, for simulating data changing ONLY
+    setTimeout(() => {
+        details.stage = 0;
+        const int = setInterval(() => {
+            details.stage++;
+            if(details.stage == 2) clearInterval(int)
+        }, 500);
+    }, 2000)
+})
+
+// getStageStr returns a string representing the current stage of this item
+function getStageStr(stage:number): string {
     switch(stage) {
         case 0: return "IO Poller"
         case 1: return "Title Format"
@@ -98,7 +134,8 @@ const getStageStr = function(stage:number): string {
     }
 }
 
-const getStatusStr = function(status:number): string {
+// getStatusStr returns a string representing the current status of this item
+function getStatusStr(status:number): string {
     switch(status) {
         case 0: return "Pending"
         case 1: return "Working"
@@ -107,34 +144,38 @@ const getStatusStr = function(status:number): string {
     }
 }
 
-const handleStatClick = function() {
+// handleStatClick will switch the component page to the TROUBLE
+// page IF the queue item is currently troubled.
+// If it's not troubled, the page is set to the page for the current stage
+function handleStatClick():void {
     if(details.trouble) {
         page = ComponentPage.TROUBLE
-    }
-}
 
-const handleSpinnerClick = function() {
-    if(details.trouble) {
-        page = ComponentPage.TROUBLE;
         return
     }
 
     page = details.stage as ComponentPage
 }
 
-const handleStageClick = function(event:CustomEvent) {
-    page = event.detail
+// handleStageClick will set the page to the event detail
+// passed to this method - this function is called when
+// we receive the 'stage-click' custom event from a child component
+function handleStageClick(event:CustomEvent) {
+    page = event.detail as ComponentPage
 }
 
+// stat is a dynamic binding method for Svelte that will live update
+// the status text based on the stage and status of the item
 $:stat = function() {
     return getStageStr(details.stage) + ": " + getStatusStr(details.status)
 }
 
+// isStatActive is a dynamic svelte binding to test if the status
+// button for this component should be marked 'active'. Is active
+// if the stage is troubled AND we're viewing the trouble.
 $:isStatActive = function() {
     return details.trouble && page == ComponentPage.TROUBLE
 }
-
-onMount(getDetails)
 </script>
 
 <style lang="scss">
@@ -172,7 +213,7 @@ onMount(getDetails)
         </div>
         <main>
             {#if page == ComponentPage.OVERVIEW}
-                <OverviewPanel details={details} on:spinner-click="{handleSpinnerClick}" on:stage-click="{handleStageClick}"/>
+                <OverviewPanel details={details} on:spinner-click="{handleStatClick}" on:stage-click="{handleStageClick}"/>
             {:else if page == ComponentPage.TITLE}
                 <TitlePanel details={details}/>
             {:else if page == ComponentPage.OMDB}

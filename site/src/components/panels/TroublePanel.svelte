396e6eb137f1e535bff2003d82caf4b3c2be38a5
Harry Felton <harryfelton12@gmail.com>
2021-07-25 21:04:58 +1200

    feat: added Trouble interfaces to mirror go-server

--- site/src/components/panels/TroublePanel.svelte
+++ site/src/components/panels/TroublePanel.svelte
@@ -1,15 +1,28 @@
 <script context="module" lang="ts">
-export interface QueueTroubleDetails extends QueueTroubleInfo {
-
+export enum QueueTroubleType {
+	TITLE_FAILURE,
+	OMDB_NO_RESULT_FAILURE,
+	OMDB_MULTIPLE_RESULT_FAILURE,
+	OMDB_REQUEST_FAILURE,
+	FFMPEG_FAILURE,
 }
+
+export interface QueueTroubleDetails {
+    trouble:QueueTroubleInfo,
+    type:QueueTroubleType,
+    expectedArgs:Object,
+    [key:string]:any
+}
+
 </script>
 
 <script lang="ts">
 import { onMount } from "svelte";
 import { commander } from "../../commander";
 import { SocketMessageType } from "../../store";
-import type { SocketData } from "../../store";
+import { QueueStage } from "../QueueItem.svelte";
 
+import type { SocketData } from "../../store";
 import type { QueueDetails, QueueTroubleInfo } from "../QueueItem.svelte";
 
 enum ComponentState {
@@ -20,18 +33,16 @@ enum ComponentState {
 
 export let details:QueueDetails
 let state = ComponentState.LOADING
-let troubleDetails:QueueTroubleInfo
+let troubleDetails:QueueTroubleDetails
 
 onMount(() => {
     commander.sendMessage({
         title: "TROUBLE_DETAILS",
         type: SocketMessageType.COMMAND,
-        arguments: {
-            id: details.id
-        }
+        arguments: { id: details.id }
     }, (data:SocketData): boolean => {
         // Wait for reply to message by using a callback.
-        if(data.title == "COMMAND_SUCCESS") {
+        if(data.title == "COMMAND_SUCCESS" && data.type == SocketMessageType.RESPONSE) {
             troubleDetails = data.arguments.payload
             state = ComponentState.COMPLETE
         } else {
@@ -44,7 +55,9 @@ onMount(() => {
 </script>
 
 <style lang="scss">
-
+.tile.trouble {
+    padding: 1rem;
+}
 </style>
 
 <!-- Template -->
@@ -54,7 +67,17 @@ onMount(() => {
     <span class="trouble">{details.trouble.message}</span>
 
     {#if state == ComponentState.COMPLETE}
-        <span>Trouble resolution fetched</span>
+        {#if details.stage == QueueStage.TITLE}
+            <span></span>
+        {:else if details.stage == QueueStage.OMDB}
+            <!-- Figure out what kind of trouble we're dealing with -->
+        {:else if details.stage == QueueStage.FFMPEG}
+            <span>FFMPEG trouble resolution</span>
+        {:else if details.stage == QueueStage.DB}
+            <span>Database trouble</span>
+        {:else}
+            <span>This stage has no known trouble resolution methods. Please consult the server logs for more information.</span>
+        {/if}
     {:else if state == ComponentState.LOADING}
         <span>Fetching trouble resolution</span>
     {:else}

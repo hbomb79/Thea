8c4ddfe35d722f5891b8a746abaf61dcbde6683e
Harry Felton <harryfelton12@gmail.com>
2023-06-05 13:45:15 +1200

    fix(ui): dynamic form crash due to unknown value types

--- website/src/components/DynamicForm.svelte
+++ website/src/components/DynamicForm.svelte
@@ -1,58 +1,58 @@
 <script lang="ts">
-import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher } from "svelte";
 
-export let fields: Object
+    export let fields: Object;
 
-const dispatch = createEventDispatcher();
-const defaultValue = (type:string, val:any): any => {
-    switch(type) {
-        case 'bool':
-            return val || false
-        case 'int':
-            return parseInt(val) || -1
-        case 'string':
-            return val || ""
-        default:
-            return ''
-    }
-}
+    const dispatch = createEventDispatcher();
+    const defaultValue = (type: string, val: any): any => {
+        switch (type) {
+            case "bool":
+                return val || false;
+            case "int":
+                return parseInt(val) || -1;
+            case "string":
+                return val || "";
+            default:
+                return "";
+        }
+    };
 
-const onFormSubmit = (event:Event) => {
-    const result = {}
-    Object.entries(fields).forEach((val:Object) => {
-        const target = event.target[val[0]]
-        result[val[0]] = defaultValue(val[1], target.type == "checkbox" ? target.checked : target.value)
-    })
+    const onFormSubmit = (event: Event) => {
+        const result = {};
+        Object.entries(fields).forEach((val: Object) => {
+            const target = event.target[val[0]];
+            if (target === undefined) return;
 
-    event.stopPropagation()
-    event.preventDefault()
+            result[val[0]] = defaultValue(val[1], target.type == "checkbox" ? target.checked : target.value);
+        });
 
-    dispatch("submitted", result)
-}
+        event.stopPropagation();
+        event.preventDefault();
 
+        dispatch("submitted", result);
+    };
 </script>
-
-<style lang="scss">
-</style>
 
 <div class="dynamic-form">
     <form action="#" on:submit={onFormSubmit}>
-        {#each Object.entries(fields) as [name, type] }
+        {#each Object.entries(fields) as [name, type]}
             <div class="field">
-                <span>{name} <i>({type})</i></span>
-
                 {#if type == "int"}
-                    <input type="number" name="{name}"/>
+                    <span>{name} <i>({type})</i></span>
+                    <input type="number" {name} />
                 {:else if type == "bool"}
-                    <input type="checkbox" name="{name}"/>
+                    <span>{name} <i>({type})</i></span>
+                    <input type="checkbox" {name} />
                 {:else if type == "string"}
-                    <input type="text" name="{name}"/>
-                {:else}
-                    <b>Cannot create dynamic form element for {type} type</b>
+                    <span>{name} <i>({type})</i></span>
+                    <input type="text" {name} />
                 {/if}
             </div>
         {/each}
 
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
     </form>
 </div>
+
+<style lang="scss">
+</style>
